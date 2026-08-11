c010ad210c60224bd29e9e1f3aa90c9384649506
zamarrud <zj1003@yahoo.com>
2026-08-11 12:31:27 +0200

    test(wpf): fix status message assertion for AssignSalespersonAsync test

---
+++ tests/Neas.SalesManager.Wpf.Tests/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Neas.SalesManager.Wpf.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // Allows Moq to mock internal interfaces
